60d90d0032fcdccc555cd553f1279dea23e90985
Raheleh sepehri <raheleh1392@gmail.com>
2022-04-21 14:05:09 +0430

    commit test code3

--- js/index.js
+++ js/index.js
@@ -2,6 +2,6 @@ const rate = document.querySelectorAll(".rate");
 
 const selected = rate.forEach((r) => {
   r.addEventListener("click", () => {
-    sessionStorage.setItem("selectedItem", JSON.stringify(r.innerHTML));
+    sessionStorage.setItem("selectedItem", r.innerHTML);
   });
 });
--- js/result.js
+++ js/result.js
@@ -1,8 +1,3 @@
 const result = document.querySelector("#selected");
 
-// console.log(sessionStorage.getItem("selectedItem"));
-if (sessionStorage.getItem("selectedItem") !== null) {
-  var selectedItem = JSON.parse(sessionStorage.getItem("selectedItem"));
-  result.innerHTML = sessionStorage.getItem("selectedItem");
-  console.log(selectedItem);
-}
+result.innerHTML = sessionStorage.getItem("selectedItem");
