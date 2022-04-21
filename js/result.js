8d77ccd02266fc1031d36a20bc9af24681faf576
Raheleh sepehri <raheleh1392@gmail.com>
2022-04-21 13:54:19 +0430

    commit cheke code2

--- js/result.js
+++ js/result.js
@@ -1,8 +1,8 @@
 const result = document.querySelector("#selected");
 
-result.innerHTML = sessionStorage.getItem("selectedItem");
 // console.log(sessionStorage.getItem("selectedItem"));
 if (sessionStorage.getItem("selectedItem") !== null) {
   var selectedItem = JSON.parse(sessionStorage.getItem("selectedItem"));
+  result.innerHTML = sessionStorage.getItem("selectedItem");
   console.log(selectedItem);
 }
